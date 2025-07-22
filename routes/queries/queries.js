6fe734660ca07ca3ed19b2070909a29ee03ea9eb
ChandaB <chandabb@aol.com>
2025-07-22 10:51:24 -0400

    Formatting updates

--- routes/queries/queries.js
+++ routes/queries/queries.js
@@ -2,11 +2,11 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.send("GET called");    
+    res.send("GET called");
 });
 
 router.post('/', async (req, res) => {
-    res.send("POST called");  
+    res.send("POST called");
 });
 
 export default router;
